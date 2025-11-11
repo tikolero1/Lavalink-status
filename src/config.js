2d9a5287aa732a6595dc245da5bae9ce5799d98c
tikolero1 <168551149+tikolero1@users.noreply.github.com>
2025-11-12 01:12:00 +0800

    updated host details

--- src/config.js
+++ src/config.js
@@ -7,9 +7,9 @@ module.exports = {
 
   nodes: [
     {
-      host: "", // Your lavalink host address 
-      password: "Titekobig123", // Your lavalink password
-      port: 2333, // Your lavalink port
+      host: "lavalink.jirayu.net", // Your lavalink host address 
+      password: "youshallnotpass", // Your lavalink password
+      port: 13592, // Your lavalink port
       identifier: "Node", // Name for your lavalink
       secure: false, // set to true if your lavalink has SSL
       reconnectTimeout: 300000,
